6eda396505a7cf8b9bfac47465f7eb0c798276b8
ChenJ <535699028@example.com>
2019-06-11 18:13:26 +0800

    add the answer of ch2

--- Chapter_2.cpp
+++ Chapter_2.cpp
@@ -1,175 +1,91 @@
-#include <iostream>
-#include <cstdarg>
-
-using namespace std;
-
-//链表结点
+/**P45 习题3
+ * 设线性表中的数据元素以值递增排列，并以单链表作为存储结构。
+ * 设计一个高效的算法，删除表中所有值大于min且小于max的元素，同时释放被删除结点的空间。
+ * 并分析算法的时间复杂度
+ */
 template <class T>
-class Link
+bool lnKList<T>::ListDelete(T min, T max)
 {
-  public:
-    T data;
-    Link<T> *next;
-    Link(Link<T> *nextValue = NULL)
+    //带头结点的非降序链表删除min<且<max的结点
+    Link<T> *p, *q, *temp;
+    q = head;
+    p = head->next;
+    //让p定位到第一个被删结点
+    while (p->data <= min)
     {
-        this->next = nextValue;
+        p = q;
+        p = p->next;
     }
-    Link(T info, Link<T> *nextValue = NULL)
+    while (p->data > min && p->data < max)
     {
-        this->data = info;
-        this->next = nextValue;
+        temp = p;
+        q->next = p->next;
+        p = p->next;
+        delete temp;
     }
-};
-
-//带头结点的链表
-template <class T>
-class LnkList
-{
-  private:
-    Link<T> *head; //头结点
-  public:
-    LnkList() //无参构造函数
-    {
-        head = new Link<T>();
-    }
-    LnkList(const initializer_list<T> &links) //有参构造函数，用了C++11特性
-    {
-        head = new Link<T>();
-        Link<T> *p = head;
-        for (auto link : links)
-        {
-            Link<T> *temp = new Link<T>(link);
-            p->next = temp;
-            p = p->next;
-        }
-    }
-    ~LnkList() //析构函数
-    {
-        Link<T> *p1, *p2;
-        p1 = head;
-        while (p1 != NULL)
-        {
-            p2 = p1;
-            p1 = p1->next;
-            delete p2;
-        }
-        delete head;
-    }
-    Link<T> *getHead()
-    {
-        return head;
-    }
-    bool append(const T data) //在链表尾部添加元素
-    {
-        Link<T> *p = head;
-        while (p->next != NULL)
-        {
-            p = p->next;
-        }
-        Link<T> *temp = new Link<T>(data);
-        p->next = temp;
-        return true;
-    }
-};
-
-//P45 第3题
-template <class T>
-void delectValue(LnkList<T> *list)
-{
-
-    Link<T> *p1;
-    p1 = list->getHead()->next;
-    int count = 0; //计算结点个数,防止误删
-    while (p1 != NULL)
-    {
-        count++;
-        p1 = p1->next;
-    }
-    if (count <= 2) //当结点数小于等于2时直接返回
-    {
-        return;
-    }
-    Link<T> *p2;
-    p1 = list->getHead()->next->next;
-    while (p1->next != NULL)
-    {
-        p2 = p1;
-        p1 = p1->next;
-        delete p2;
-    }
-    list->getHead()->next->next = p1;
+    return true;
 }
+//时间复杂度：O(n)
+//当时想得太浅，链表中可能存在相同数值的数，所以需要进行比较
 
-//P45 第8题
+/**P45 8
+ * 已知两个元素按值增值有序排列的线性表A和B，且同一表中的元素值各不相同。
+ * 试构造一个线性表C，其元素为A和B中元素的交集，且表C中的元素也按值递增有序排列。
+ * 设计对A、B、C都是顺序表时的算法。
+ */
+//老师的答案太绕了，我直接用我的改了
 template <class T>
 T *intersection(T *a, T *b, int lenA, int lenB)
 {
     T *c = new T[lenA > lenB ? lenB : lenA];
-    int k = 0;
-    for (int i = 0; i < lenA; i++)
+    int i = j = k = 0;
+    while (i < lenA && j < lenB)
     {
-        for (int j = 0; j < lenB; j++)
+        if (a[i] > b[j])
         {
-            if (b[j] > a[i])
-                break;
-            else if (b[j] == a[i])
-            {
-                c[k++] = b[j];
-                break;
-            }
+            j++;
+        }
+        else if (a[i] < b[j])
+        {
+            i++;
+        }
+        else
+        {
+            c[k++] = a[i++];
+            j++;
         }
     }
     return c;
 }
 
-//P45 第9题
-template <class T>
-LnkList<T> *intersection(LnkList<T> *listA, LnkList<T> *listB)
+/**P45 9
+ * 同上题，设计当A、B、C都是单链表的算法
+ */
+//和第8题的思路是一样的
+InkList<T> InkList<T>::Intersection(InkList<T> A, InkList<T> B)
 {
-    LnkList<T> *listC = new LnkList<T>();
-    Link<T> *pa = listA->getHead()->next;
-    while (pa != NULL)
+    InkList<T> *C, *p, *q, *r;
+    p = A.head->next;
+    q = B.head->next;
+    C.head = C.tail = new Link();
+    while (p != NULL && q != NULL)
     {
-        Link<T> *pb = listB->getHead()->next;
-        while (pb != NULL)
+        if (p->data < q->data)
         {
-            if (pb->data == pa->data)
-            {
-                listC->append(pb->data);
-                break;
-            }
-            pb = pb->next;
+            p = p->next;
         }
-        pa = pa->next;
+        else if (p->data > q->data)
+        {
+            q = q->next;
+        }
+        else
+        {
+            r = new Link(p->data, NULL);
+            C.tail->next = r;
+            C.tail = r;
+            p = p->next;
+            q = q->next;
+        }
+        return C;
     }
-    return listC;
-}
-
-template <class T>
-void showLnkList(LnkList<T> *list)
-{
-    Link<T> *p = list->getHead()->next;
-    while (p != NULL)
-    {
-        cout << p->data << endl;
-        p = p->next;
-    }
-}
-
-int main()
-{
-    // LnkList<int> *test1 = new LnkList<int>({1,2,3});
-    // showLnkList(test1);
-    // delectValue(test1);
-    // showLnkList(test1);
-    LnkList<int> *listA = new LnkList<int>({1, 2, 3, 4, 5, 6});
-    LnkList<int> *listB = new LnkList<int>({2, 3, 4, 5, 6, 7});
-    LnkList<int> *listC = intersection(listA, listB);
-    cout<<"A"<<endl;
-    showLnkList(listA);
-    cout<<"B"<<endl;
-    showLnkList(listB);
-    cout<<"C"<<endl;
-    showLnkList(listC);
-    return 0;
 }
